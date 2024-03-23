3c448ccbdd223d3f1ef3c33610c54b62c01ef2a6
caffae <wangyeelin@gmail.com>
2024-03-23 13:27:20 +0800

    fixing up my release script

--- prepareForGitRelease.sh
+++ prepareForGitRelease.sh
@@ -2,17 +2,19 @@
 # npm version patch
 
 # create the current_release directory if it does not exist
-mkdir -p release
+mkdir -p current-folder-notes
 
 # make a copy of the main.js, manifest.json, and styles.css files in another folder
-cp main.js release
-cp manifest.json release
-cp styles.css release
+cp main.js current-folder-notes
+cp manifest.json current-folder-notes
+cp styles.css current-folder-notes
 
 # compress the current_release folder into a zip file
 # zip -r release.zip current_release
-zip -vr release.zip release -x "*.DS_Store"
+zip -vr current-folder-notes.zip current-folder-notes -x "*.DS_Store"
+
+mv current-folder-notes.zip release.zip
 
 # remove the current_release folder
-rm -rf release
+# rm -rf current-folder-notes
 
